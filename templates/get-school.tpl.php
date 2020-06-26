9ae73c7a2ca29fdea01ae2c1b0af72127bf6f5e5
qidian <qidian990107@163.com>
2020-06-26 02:22:37 -0700

    Add template files

---
+++ templates/get-school.tpl.php
@@ -0,0 +1,22 @@
+<div class="school-name"><?php echo $school->name ?></div>
+
+<div class="school-field">
+  <div class="school-field__label">School ID</div>
+  <div class="school-field__value"><?php echo $school->sid ?></div>
+</div>
+<div class="school-field">
+  <div class="school-field__label">School Address</div>
+  <div class="school-field__value"><?php echo $school->address ?></div>
+</div>
+<div class="school-field">
+  <div class="school-field__label">Zip Code</div>
+  <div class="school-field__value"><?php echo $school->zip_code ?></div>
+</div>
+
+<div class="buttons">
+  <button class="edit-button" onclick="window.location.href='/schools/<?php echo $school->sid ?>/edit'">Edit</button>
+  <button class="delete-button" onclick="window.location.href='/schools/<?php echo $school->sid ?>/delete'">Delete</button>
+</div>
+<?php
+
+// dpm($delete_cb);
